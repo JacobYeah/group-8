20556f223e7db6494d142e2e0e64c32212cc31bb
Ming <lumingabby@outlook.com>
2020-03-13 03:14:31 -0400

    update homepage

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,8 +1,21 @@
 <template>
     <div>
-    <b-jumbotron header="Recommend for you"></b-jumbotron>
+    <b-jumbotron header="Recommend for you">
+      <b-button variant="primary" @click="get_recommend()">refresh</b-button>
+    </b-jumbotron>
 
     <b-container fluid="sm">
+      <div class="person-wrap" ref="personWrap">
+          <ul>
+            <li v-for="product in recomms" :key="product">
+              <p>{{product}}</p>
+
+            </li>
+          </ul>
+      </div>
+    </b-container>
+
+    <!-- <b-container fluid="sm">
       <b-carousel
         id="carousel-1"
         v-model="slide"
@@ -18,21 +31,18 @@
       >
 
       <ul>
-        <li v-for="item in items" :key="item.title">
+        <li v-for="product in recomms" :key="product">
           <b-carousel-slide
             img-src="https://picsum.photos/1024/480/?image=58"
           >
             <b-carousel-slide-caption>
-              <h2>{{item.title}}</h2>
+              <h2>{{product}}</h2>
             </b-carousel-slide-caption>
-            <b-carousel-slide-text>
-              {{item.description}}
-            </b-carousel-slide-text>
           </b-carousel-slide>
         </li>
       </ul>
       </b-carousel>
-    </b-container>
+    </b-container> -->
 
     <b-jumbotron header="All Products"></b-jumbotron>
 
@@ -42,7 +52,6 @@
           <b-card img-src="https://placekitten.com/g/300/450" img-alt="Card image" img-left class="mb-3">
             <b-card-text>
               <h2>{{item.title}}</h2>
-              <p>{{item.description}}</p>
             </b-card-text>
 
             <b-button href="#" variant="primary">buy</b-button>
@@ -54,8 +63,10 @@
 </template>
 
 <script>
+import axios from 'axios';
+import BScroll from "better-scroll";
+
 export default {
-  name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
@@ -65,9 +76,15 @@ export default {
         {title: 'item3', description: 'item3-description'},
         {title: 'item4', description: 'item4-description'}
       ],
+      recomms: ['p1','p2','p3'],
       slide: 0,
-      sliding: null
+      sliding: null,
     }
+  },
+  created() {
+    this.$nextTick(() => {
+      this.personScroll();
+    });
   },
   methods: {
     onSlideStart (slide) {
@@ -75,6 +92,39 @@ export default {
     },
     onSlideEnd (slide) {
       this.sliding = false
+    },
+    get_recommend() {
+      const getRecommend_url = 'http://localhost:5000/'
+      axios.get(getRecommend_url, {
+                    params: {
+                        customer_id: '1'
+                    }
+                })
+                .then((response) => {
+                                    console.log(this.recomms);
+                  this.recomms = response.data;
+                  console.log(this.recomms);
+                })
+                .catch(function (error) {
+                  console.log(error);
+                });
+    },
+     personScroll() {
+      let width = 6 * 120;
+      this.$refs.personTab.style.width = width + "px";
+      this.$nextTick(() => {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.personWrap, {
+            startX: 0,
+            click: true,
+            scrollX: true,
+            scrollY: false,
+            eventPassthrough: "vertical"
+          });
+        } else {
+          this.scroll.refresh();
+        }
+      });
     }
   }
 }
